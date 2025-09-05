a9466dd71ca41a9a8a1ef41b95f6f2dd9ddb1a28
ihugo1 <huglainez@gmail.com>
2025-09-05 12:54:38 -0600

    Create ScrollToTop component

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { App } from "./App.tsx";
 import "./main.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // <-- Asegúrate de tener estas importaciones
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const queryClient = new QueryClient(); // <-- Define tu QueryClient aquí
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </QueryClientProvider>
